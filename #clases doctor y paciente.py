a7fe8bcdbceaa69eb5a1a03ec4a8f41660568ea2
ybamBogado <144073157+ybamBogado@users.noreply.github.com>
2024-06-12 02:32:30 -0300

    Update #clases doctor y paciente.py
    comente las clases que no implemente totalmente

--- #clases doctor y paciente.py
+++ #clases doctor y paciente.py
@@ -1,15 +1,15 @@
 #clases doctor y paciente
 #patient_name, patient_surname,patient_id, doctor_major
-class patient:
-    def __init__(self, patient_name, patient_surname, patient_id, doctor_major):
-        self.patient_name = patient_name
-        self.patient_surname = patient_surname
-        self.patient_id = patient_id
-        self.doctor_major=doctor_major
+# class patient:
+#     def __init__(self, patient_name, patient_surname, patient_id, doctor_major):
+#         self.patient_name = patient_name
+#         self.patient_surname = patient_surname
+#         self.patient_id = patient_id
+#         self.doctor_major=doctor_major
 
-class doctor:
-    def __init__(self, doctor_name, doctor_surname, doctor_id, major):
-        self.doctor_name = doctor_name
-        self.doctor_surname = doctor_surname
-        self.doctor_id = doctor_id
-        self.major=major
+# class doctor:
+#     def __init__(self, doctor_name, doctor_surname, doctor_id, major):
+#         self.doctor_name = doctor_name
+#         self.doctor_surname = doctor_surname
+#         self.doctor_id = doctor_id
+#         self.major=major
